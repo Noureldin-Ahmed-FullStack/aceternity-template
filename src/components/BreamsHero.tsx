6c8400e775d494eb6bbbadcd47bde643ba30fcd4
Noureldin-Ahmed-FullStack <noureldin2662002@gmail.com>
2024-10-05 09:10:03 +0300

    updated

--- src/components/BreamsHero.tsx
+++ src/components/BreamsHero.tsx
@@ -1,6 +1,23 @@
 import { BackgroundBeams } from "./ui/background-beams";
+import { PlaceholdersAndVanishInput } from "./ui/placeholders-and-vanish-input";
 
 export function BeamsHero() {
+  const placeholders = [
+    "What's the first rule of Fight Club?",
+    "Who is Tyler Durden?",
+    "Where is Andrew Laeddis Hiding?",
+    "Write a Javascript method to reverse a string",
+    "How to assemble your own PC?",
+  ];
+  const handleChange = (_e: React.ChangeEvent<HTMLInputElement>) => {
+    // console.log(e.target.value);
+  };
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const form = e.target as HTMLFormElement;
+    const firstInputValue = (form.elements[0] as HTMLInputElement).value;
+    console.log(firstInputValue);
+  };
   return (
     <div className="h-[40rem] w-full rounded-md bg-neutral-950 relative flex flex-col items-center justify-center antialiased">
       <div className="max-w-2xl mx-auto p-4">
@@ -15,11 +32,14 @@ export function BeamsHero() {
           password reset emails, or promotional campaigns, MailJet has got you
           covered.
         </p>
-        <input
-          type="text"
-          placeholder="hi@manuarora.in"
-          className="rounded-lg border p-2 border-neutral-800 focus:ring-2 focus:ring-teal-500  w-full relative z-10 mt-4  bg-neutral-950 placeholder:text-neutral-700"
-        />
+        <div className="mt-6">
+
+            <PlaceholdersAndVanishInput
+              placeholders={placeholders}
+              onChange={handleChange}
+              onSubmit={onSubmit}
+            />
+        </div>
       </div>
       <BackgroundBeams />
     </div>
